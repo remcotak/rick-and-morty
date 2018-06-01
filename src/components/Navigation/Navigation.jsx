6a8a77ba0790282f944b93a4b53706d90cafa7dc
rtak <rtak@e-sites.nl>
2018-06-01 14:51:10 +0200

    Navigation now has a total characters count

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,41 +1,52 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { withRouter, NavLink } from 'react-router-dom';
 import styles from './Navigation.css';
 
-const Navigation = props => (
-  <nav styleName="navigation">
-    {console.log(props)}
-    <ul styleName="navigation__list">
-      <li>
-        <NavLink
-          styleName="navigation__link"
-          exact
-          to="/"
-          activeClassName={styles.active}
-        >
-          Characters ({props.charactersCount || '0'})
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          styleName="navigation__link"
-          to="/locations"
-          activeClassName={styles.active}
-        >
-          Locations
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          styleName="navigation__link"
-          to="/episodes"
-          activeClassName={styles.active}
-        >
-          Episodes
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+class Navigation extends Component {
+  render() {
+    return (
+      <nav styleName="navigation">
+        <ul styleName="navigation__list">
+          <li>
+            <NavLink
+              styleName="navigation__link"
+              exact
+              to="/"
+              activeClassName={styles.active}
+            >
+              Characters ({this.props.charactersCount || '0'})
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              styleName="navigation__link"
+              to="/locations"
+              activeClassName={styles.active}
+            >
+              Locations
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              styleName="navigation__link"
+              to="/episodes"
+              activeClassName={styles.active}
+            >
+              Episodes
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
-export default Navigation;
+export default withRouter(
+  connect(
+    state => ({
+      charactersCount: state.characters.info.count
+    }),
+    {}
+  )(Navigation)
+);
